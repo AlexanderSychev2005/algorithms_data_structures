9169ba60a41a31fb0153df25a312166c46cb135a
Alex <alexandersychev2005@gmail.com>
2025-08-30 11:08:57 +0300

    Updated queue implementation

--- queue_implementation.py
+++ queue_implementation.py
@@ -2,6 +2,51 @@
 
 import collections
 
+
+# dq = collections.deque([1, 2, 3, 4, 5], maxlen=5)
+# dq.append(6)  # The oldest item is removed
+# print(dq)
+# dq.appendleft(0)  # The oldest item is removed
+# print(dq)
+# dq.pop()
+# print(dq)
+# dq.popleft()
+# print(dq)
+#
+# dq = collections.deque()
+# try:
+#     dq.pop()
+# except IndexError as e:
+#     print(e)
+
+# dq = collections.deque([1, 2, 3, 4, 5])
+# dq.extend([6, 7, 8])
+# dq.extendleft([0, -1, -2])
+# dq.insert(0, 100)
+# dq.insert(100, 100)
+# print(dq)
+# dq.remove(100)
+# print(dq)
+# dq.clear()
+# print(dq)
+#
+# dq2 = dq.copy()
+
+# FIFO (First In, First Out) Queue Implementation
+dq = collections.deque([1, 2, 3, 4, 5])
+dq.appendleft(10)
+value = dq.pop()
+
+dq.append(10)
+value = dq.popleft()
+
+# LIFO (Last In, First Out) Stack Implementation
+dq.append(10)
+value = dq.pop()
+
+dq.appendleft(10)
+value = dq.popleft()
+
 q = collections.deque()
 
 
